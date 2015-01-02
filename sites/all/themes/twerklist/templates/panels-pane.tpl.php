7b8ca077f84555fb73562b2117e7218e6f85b725
Cornel Andreev <hugronaphor@gmail.com>
2015-01-02 23:45:33 +0000

    More styles.

--- sites/all/themes/twerklist/templates/panels-pane.tpl.php
+++ sites/all/themes/twerklist/templates/panels-pane.tpl.php
@@ -27,7 +27,7 @@
 
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
-    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+  <h2<?php print $title_attributes; ?>><span><?php print $title; ?></span></h2>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
 
